584a2c9543bb14dadafc0cfb2147db6df9592976
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-03-27 16:30:28 +1000

    Fix error message for let options_38ff418 = {};

--- WebExtension/extension/content-setting.js
+++ WebExtension/extension/content-setting.js
@@ -1,27 +1,30 @@
-let options_38ff418 = {};
+(function() {
+  let options_38ff418 = {};
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // The service worker wants to get a setting
-    if (Object.hasOwn(request, "getSetting")) {
-        if (!Object.hasOwn(options_38ff418, request.setting)) {
-            sendResponse({ value: false });
-            return;
-        }
-        sendResponse({ value: options_38ff418[request.setting] });
-        return;
-    }
-    if (Object.hasOwn(request, "getSettings")) {
-        sendResponse(options_38ff418);
-        return;
-    }
-    if (Object.hasOwn(request, "putSetting")) {
-        options_38ff418[request.setting] = request.value;
-        return;
-    }
-    if (Object.hasOwn(request, "putSettings")) {
-        options_38ff418 = request.value;
-        return;
-    }
-});
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      // The service worker wants to get a setting
+      if (Object.hasOwn(request, "getSetting")) {
+          if (!Object.hasOwn(options_38ff418, request.setting)) {
+              sendResponse({ value: false });
+              return;
+          }
+          sendResponse({ value: options_38ff418[request.setting] });
+          return;
+      }
+      if (Object.hasOwn(request, "getSettings")) {
+          sendResponse(options_38ff418);
+          return;
+      }
+      if (Object.hasOwn(request, "putSetting")) {
+          options_38ff418[request.setting] = request.value;
+          return;
+      }
+      if (Object.hasOwn(request, "putSettings")) {
+          options_38ff418 = request.value;
+          return;
+      }
+  });
+})();
+
 
 undefined;
