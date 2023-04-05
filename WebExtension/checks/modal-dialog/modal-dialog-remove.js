73b5618f14ffbb215f23dbbee5873959f6864ea7
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-04-06 08:47:17 +1000

    Remove aria-label check

--- WebExtension/checks/modal-dialog/modal-dialog-remove.js
+++ WebExtension/checks/modal-dialog/modal-dialog-remove.js
@@ -2,8 +2,6 @@ function removeInjectedDivs() {
     const messageClasses = [
       'missing-aria-modal-776553',
       'invalid-accessible-name-776553',
-      'dialog-in-inert-layer-776553',
-      'inert-layer-missing-aria-hidden-776553',
     ];
     messageClasses.forEach((messageClass) => {
       const messageDivs = document.querySelectorAll(`.${messageClass}`);
