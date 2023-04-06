dae6ca4d9e8bbe392254577be39b8635aab70a43
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-04-07 07:59:19 +1000

    Add new target size function checks

--- WebExtension/checks/target-size/target-size-remove.js
+++ WebExtension/checks/target-size/target-size-remove.js
@@ -1,12 +1,21 @@
-// Get all elements with the small-target class
-document.querySelectorAll('.small-target')
-// Loop through each element and remove the class
-  .forEach(elem => {
-  // Remove the div container if it exists
-  const targetSizeElem = elem.nextElementSibling;
-  if (targetSizeElem && targetSizeElem.classList.contains('target-size-8228965')) {
-    targetSizeElem.remove();
-  }
-  // Remove the small-target class
-  elem.classList.remove('small-target');
-});
+function removeInjectedDivs() {
+  const messageClasses = [
+    'target-size-8228965',
+    'circle-shape-8228965',
+  ];
+
+  messageClasses.forEach((messageClass) => {
+    const messageDivs = document.querySelectorAll(`.${messageClass}`);
+
+    messageDivs.forEach((messageDiv) => {
+      if (messageClass === 'circle-shape-8228965') {
+        // Remove the pos-rel-8228965 class from the parent element
+        messageDiv.parentNode.classList.remove('pos-rel-8228965');
+      }
+      messageDiv.remove();
+    });
+  });
+}
+
+// Call the function
+removeInjectedDivs();
