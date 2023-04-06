b8caee7f0cb7201bdaad0af130dc6d0b2edbc8ab
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-04-07 09:50:08 +1000

    Update combobox check for positioning elements

--- WebExtension/checks/combobox/combobox.js
+++ WebExtension/checks/combobox/combobox.js
@@ -1,3 +1,10 @@
+// Create a global wrapper div for messages
+if (typeof wrapperDiv === 'undefined') {
+  var wrapperDiv = document.createElement('div');
+  wrapperDiv.classList.add('container-776553');
+}
+
+// Create a message div with specified class and message text
 function createMessageDiv(messageClass, message) {
   const messageDiv = document.createElement('div');
   messageDiv.classList.add(messageClass);
@@ -6,47 +13,67 @@ function createMessageDiv(messageClass, message) {
   return messageDiv;
 }
 
+// Insert a message div into the global wrapper div and place it after the target element
+function insertMessageDiv(target, messageDiv) {
+  const messageClass = messageDiv.className;
+
+  // Check if a div with the same message class already exists in the wrapper div
+  const existingMessageDiv = wrapperDiv.querySelector(`.${messageClass}`);
+
+  // If a div with the same message class does not exist, insert the new message div
+  if (!existingMessageDiv) {
+    wrapperDiv.append(messageDiv);
+    target.after(wrapperDiv);
+  }
+}
+
+// Check ARIA attributes for all combobox inputs
 function checkComboboxListAttributes() {
+  // Clear the content of the global wrapper div
+  wrapperDiv.innerHTML = '';
+
   const comboboxInputs = document.querySelectorAll('input[role="combobox"]');
   checkAriaActiveDescendant(comboboxInputs);
   checkAriaExpanded(comboboxInputs);
-  checkAriaControls();
+  checkAriaControls(comboboxInputs);
   checkAriaAutocomplete(comboboxInputs);
   checkAriaOwnsUsage(comboboxInputs);
 }
 
+// Check if the "aria-expanded" attribute is present on combobox inputs
 function checkAriaExpanded(comboboxInputs) {
   comboboxInputs.forEach((input) => {
     const ariaExpanded = input.getAttribute('aria-expanded');
     if (ariaExpanded === null) {
       const messageDiv = createMessageDiv('missing-aria-expanded-776553', 'The combobox input should have the "aria-expanded" attribute');
-      input.after(messageDiv);
+      insertMessageDiv(input, messageDiv);
     }
   });
 }
 
-function checkAriaControls() {
-  const inputs = document.querySelectorAll('input[role="combobox"]');
-
-  inputs.forEach((input) => {
-    const ariaExpanded = input.getAttribute('aria-controls');
-    if (ariaExpanded === null) {
+// Check if the "aria-controls" attribute is present on combobox inputs
+function checkAriaControls(comboboxInputs) {
+  comboboxInputs.forEach((input) => {
+    const ariaControls = input.getAttribute('aria-controls');
+    if (ariaControls === null) {
       const messageDiv = createMessageDiv('missing-aria-controls-776553', 'The combobox input should have the "aria-controls" attribute');
-      input.after(messageDiv);
+      insertMessageDiv(input, messageDiv);
     }
   });
 }
 
+// Check if the "aria-autocomplete" attribute is present on combobox inputs
 function checkAriaAutocomplete(comboboxInputs) {
   comboboxInputs.forEach((input) => {
     const ariaAutocomplete = input.getAttribute('aria-autocomplete');
     if (ariaAutocomplete === null) {
       const messageDiv = createMessageDiv('missing-aria-autocomplete-776553', 'The combobox input should have the "aria-autocomplete" attribute');
-      input.after(messageDiv);
+      insertMessageDiv(input, messageDiv);
     }
   });
 }
 
+// Check if the "aria-activedescendant" attribute is set correctly on combobox inputs when "aria-expanded" is true
 function checkAriaActiveDescendant(comboboxInputs) {
   comboboxInputs.forEach((input) => {
     const ariaExpanded = input.getAttribute('aria-expanded');
@@ -55,21 +82,22 @@ function checkAriaActiveDescendant(comboboxInputs) {
     if (ariaExpanded === 'true') {
       if (ariaActiveDescendant !== null && ariaActiveDescendant !== '') {
         const messageDiv = createMessageDiv('aria-conditions-met-776553', 'All conditions are met');
-        input.after(messageDiv);
+        insertMessageDiv(input, messageDiv);
       } else {
         const messageDiv = createMessageDiv('missing-aria-activedescendant-776553', 'Check that aria-activedescendant attribute is set to the id of the role option that has aria-selected=true');
-        input.after(messageDiv);
+        insertMessageDiv(input, messageDiv);
       }
     }
   });
 }
 
+// Check if the "aria-owns" attribute is being used correctly and if ARIA 1.0 or 1.1 patterns are being used
 function checkAriaOwnsUsage(comboboxInputs) {
   comboboxInputs.forEach((input) => {
     const ariaOwns = input.getAttribute('aria-owns');
     if (ariaOwns !== null) {
       const messageDiv = createMessageDiv('unsupported-aria-1_0-776553', 'It looks like you are using the unsupported ARIA 1.0 pattern. Please update to ARIA 1.2');
-      input.after(messageDiv);
+      insertMessageDiv(input, messageDiv);
     }
   });
 
@@ -79,10 +107,10 @@ function checkAriaOwnsUsage(comboboxInputs) {
     const ariaOwns = container.getAttribute('aria-owns');
     if (ariaOwns !== null) {
       const messageDiv = createMessageDiv('unsupported-aria-1_1-776553', 'It looks like you are using the unsupported ARIA 1.1 pattern. Please update to ARIA 1.2');
-      container.after(messageDiv);
+      insertMessageDiv(container, messageDiv);
     }
   });
 }
-  
- // Call the checkComboboxAttributes function
+
+// Call the checkComboboxAttributes function
 checkComboboxListAttributes();
