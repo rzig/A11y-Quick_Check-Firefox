aab83f87043dd5f22a4b6492b7b3c6084462baca
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-07-29 17:17:06 +1000

    Focus Order check

---
+++ WebExtension/checks/focus-order-count/focus-order-count-remove.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+for (const element of document.querySelectorAll(
+  "[data-domCount-889557]"
+)) {
+  element.removeAttribute("data-domCount-889557");
+}
+
+removeInjectedDivs([
+    'domCountDiv',
+  ]);
