e98056dc8bb8c52023c2aefc0be383d3e49fcd4a
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-08-06 20:28:08 +1000

    Part 2 Headings update

--- WebExtension/checks/headings/headings-remove.ts
+++ WebExtension/checks/headings/headings-remove.ts
@@ -1,13 +1,20 @@
 "use strict";
 
 for (const element of document.querySelectorAll(
-  ".skipped-level-555897"
+  ".skipped-level-555897, .aria-skipped-level-555897, .aria-level-missing-555897, redundant-aria-role-555897, changed-aria-level-555897, aria-missing-level-555897"
 )) {
   element.classList.remove(
-  "skipped-level-555897"
+    "skipped-level-555897",
+    "aria-skipped-level-555897",
+    "aria-level-missing-555897"
   );
 }
 
 removeInjectedDivs([
   "skipped-level-message-555897",
+  "aria-skipped-level-message-555897",
+  "aria-level-missing-message-555897",
+  "redundant-aria-role-message-555897",
+  "changed-aria-level-message-555897",
+  "aria-missing-level-message-555897",
 ]);
--- WebExtension/checks/headings/headings.ts
+++ WebExtension/checks/headings/headings.ts
@@ -1,3 +1,4 @@
+// Detect if any heading levels are skipped on the page.
 function detectSkippedHeadings() {
   const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
   let prevLevel = 0;
@@ -8,8 +9,6 @@ function detectSkippedHeadings() {
     // Check if there's a skipped level
     if (currentLevel - prevLevel > 1) {
       heading.classList.add('skipped-level-555897');
-      //heading.setAttribute('data-skipped-555897', '');
-
       const message = `(Warning) Skipped heading level from h${prevLevel} to ${heading.tagName}`;
       addMessageToPrecedingDiv(heading, 'skipped-level-message-555897', message);
     }
@@ -18,5 +17,69 @@ function detectSkippedHeadings() {
   });
 }
 
-// Run the function
+function checkRedundantARIA() {
+  const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6, [role="heading"]');
+
+  headings.forEach((heading: Element) => {
+    const htmlLevel = parseInt(heading.tagName.slice(1) || '0');
+    const ariaRole = heading.getAttribute('role');
+    const ariaLevel = heading.getAttribute('aria-level') ? parseInt(heading.getAttribute('aria-level')!) : null;
+
+    // Condition 1: Redundant ARIA Heading Role
+    if (htmlLevel && ariaRole === 'heading' && !ariaLevel) {
+      heading.classList.add('redundant-aria-role-555897');
+      const message = '(Warning) Redundant ARIA Heading Role';
+      addMessageToPrecedingDiv(heading, 'redundant-aria-role-message-555897', message);
+    }
+
+    // Condition 2: ARIA-LEVEL changes HTML heading level
+    if (htmlLevel && ariaRole === 'heading' && ariaLevel && htmlLevel !== ariaLevel) {
+      heading.classList.add('changed-aria-level-555897');
+      const message = `(Fail) Redundant ARIA Heading Role. ARIA-LEVEL changes HTML heading level from h${htmlLevel} to aria-level=${ariaLevel}`;
+      addMessageToPrecedingDiv(heading, 'changed-aria-level-message-555897', message);
+  }
+  });
+}
+
+function checkMissingARIALevel() {
+  const ariaHeadings = document.querySelectorAll('[role="heading"]:not(h1):not(h2):not(h3):not(h4):not(h5):not(h6)');
+
+  ariaHeadings.forEach((heading: Element) => {
+    const ariaLevel = heading.getAttribute('aria-level');
+
+    // Condition 3: ARIA Heading Role missing a ARIA-LEVEL
+    if (!ariaLevel) {
+      heading.classList.add('aria-missing-level-555897');
+      const message = '(Warning) ARIA Heading Role missing a ARIA-LEVEL.';
+      addMessageToPrecedingDiv(heading, 'aria-missing-level-message-555897', message);
+    }
+  });
+}
+
+function detectSkippedARIAHeadings() {
+  const ariaHeadings = document.querySelectorAll('[role="heading"]');
+  let prevLevel = 0;
+
+  ariaHeadings.forEach((heading: Element) => {
+    const ariaLevel = heading.getAttribute('aria-level');
+
+    if (ariaLevel) {
+      const currentLevel = parseInt(ariaLevel);
+
+      // Check if there's a skipped level
+      if (currentLevel - prevLevel > 1) {
+        heading.classList.add('aria-skipped-level-555897');
+        const message = `(Warning) Skipped ARIA heading level from aria-level=${prevLevel} to aria-level=${currentLevel}`;
+        addMessageToPrecedingDiv(heading, 'aria-skipped-level-message-555897', message);
+      }
+
+      prevLevel = currentLevel;
+    }
+  });
+}
+
+// Run the functions
 detectSkippedHeadings();
+detectSkippedARIAHeadings();
+checkRedundantARIA();
+checkMissingARIALevel();
