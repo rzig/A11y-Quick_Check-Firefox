accfa63b72def8cabf5563bd3afb18dd3f0a9ec8
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-08-09 08:27:13 +1000

    Update DL checks

--- WebExtension/checks/description-lists/description-list.ts
+++ WebExtension/checks/description-lists/description-list.ts
@@ -4,7 +4,7 @@ function checkDescriptionLists(): void {
   const dlElements = document.querySelectorAll("dl");
 
   for (const dlElement of dlElements) {
-    let isNested = false;
+    let failureDetected = false; // Add this flag to keep track of failures
 
     // Check for nested DL elements
     if (dlElement.parentElement && dlElement.parentElement.nodeName === 'DL') {
@@ -12,32 +12,39 @@ function checkDescriptionLists(): void {
       dlElement.setAttribute('data-isNestedList', 'true');
       const message = "Fail: Nested DL detected.";
       createChildMessageDiv(dlElement, "nested-dl-message-9927845", message);
-      isNested = true;  // Mark that a nested DL was detected
-      continue;  // Skip the remaining checks for this element
-    }
-
-    if (isNested) {
-      // If a nested DL was detected for this element, skip the other checks
       continue;
     }
 
     // Check for DIV wrapping DT and DD
-    const divChild = Array.from(dlElement.children).find((child) => child.nodeName === "DIV" && child.querySelector("dt") && child.querySelector("dd"));
+    const divChild = Array.from(dlElement.children).find((child) => 
+      child.nodeName === "DIV" && (child.querySelector("dt") && child.querySelector("dd"))
+    );
     if (divChild) {
-      dlElement.classList.add("dl--valid-div-wrap-dt-dd");
+      dlElement.classList.add("dl--valid-div-wrap-dt-dd-9927845");
       const message = "Pass: DIV wrapping DT and DD in DL is valid.";
-      createChildMessageDiv(dlElement, "div-wrap-message", message);
-      continue;  // Skip the remaining checks for this element
+      createChildMessageDiv(dlElement, "div-wrap-message-9927845", message);
+      continue;
     }
 
-    // Check for first child of DL being DIV, SPAN, or P - using these as the most common seen in the wild.
-    const firstChild = dlElement.firstElementChild;
-    if (firstChild && (firstChild.nodeName === "DIV" || firstChild.nodeName === "SPAN" || firstChild.nodeName === "P")) {
-      dlElement.classList.add("dl--invalid-first-child-9927845");
-      const message = `Fail: Element ${firstChild.nodeName} cannot be the first child of DL.`;
-      createChildMessageDiv(dlElement, "invalid-first-child-message-9927845", message);
-      continue;  // Skip the remaining checks for this element
+    // Check for invalid elements inside DL or div without dt and dd
+    for (const child of dlElement.children) {
+      if (!['DT', 'DD', 'SCRIPT', 'TEMPLATE'].includes(child.nodeName)) {
+        failureDetected = true; // Set the failure flag when an invalid child is detected
+        if (child.nodeName === "DIV" && (!child.querySelector("dt") || !child.querySelector("dd"))) {
+          dlElement.classList.add("dl--invalid-child-9927845");
+          const message = `Fail: Invalid DIV without DT and DD detected inside DL.`;
+          createChildMessageDiv(dlElement, "invalid-child-message-9927845", message);
+          break;
+        } else {
+          dlElement.classList.add("dl--invalid-child-9927845");
+          const message = `Fail: Invalid child ${child.nodeName} detected inside DL.`;
+          createChildMessageDiv(dlElement, "invalid-child-message-9927845", message);
+          break;
+        }
+      }
     }
+
+    if (failureDetected) continue; // If a failure was detected, skip the remaining checks for this element
 
     // Check for valid HTML Description List
     const hasHtmlDl = dlElement.nodeName === "DL" && !dlElement.hasAttribute("role");
@@ -45,7 +52,7 @@ function checkDescriptionLists(): void {
       dlElement.classList.add("dl--valid-html-9927845");
       const message = "Pass: Description List (HTML)";
       createChildMessageDiv(dlElement, "html-dl-message-9927845", message);
-      continue;  // Skip the remaining checks for this element
+      continue;
     }
 
     // Check if the DL element, being a parent, has an invalid role
