e807f22f22cda28750ff43df5b3306c2aced3827
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-08-09 18:29:42 +1000

    Revert "Hidden images"
    This reverts commit 2cf078f1f0fb8a8ec51be2ae8fa427088b854048.

--- WebExtension/checks/common.ts
+++ WebExtension/checks/common.ts
@@ -65,8 +65,3 @@ function removeInjectedDivs(messageClasses: string[]) {
     }
   }
 }
-
-function isHidden(element: HTMLElement): boolean {
-  const style: CSSStyleDeclaration = getComputedStyle(element);
-  return style.display === 'none' || style.visibility === 'hidden' || element.hasAttribute('hidden');
-}
