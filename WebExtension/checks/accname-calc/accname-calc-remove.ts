bee5d2fce6bc4f9ab34e076d9c9082ff1088840f
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-08-18 19:54:51 +1000

    Update aria-label check

--- WebExtension/checks/accname-calc/accname-calc-remove.ts
+++ WebExtension/checks/accname-calc/accname-calc-remove.ts
@@ -1,14 +1,19 @@
 "use strict";
 
 function removedataNameProhibited() {
-    const targets = document.querySelectorAll("[data-name-prohibited-aria], [data-name-prohibited-html]")
-    for (const noName of targets) {
-        if (noName.hasAttribute("data-name-prohibited-aria")) {
-            noName.removeAttribute("data-name-prohibited-aria")
-        }
-        if (noName.hasAttribute("data-name-prohibited-html")) {
-            noName.removeAttribute("data-name-prohibited-html")
-        }
+  const targets = document.querySelectorAll(
+    "[data-name-prohibited-aria], [data-name-prohibited-html], [data-name-permitted-aria]"
+  );
+  for (const noName of targets) {
+    if (noName.hasAttribute("data-name-prohibited-aria")) {
+      noName.removeAttribute("data-name-prohibited-aria");
     }
+    if (noName.hasAttribute("data-name-prohibited-html")) {
+      noName.removeAttribute("data-name-prohibited-html");
+    }
+    if (noName.hasAttribute("data-name-permitted-aria")) {
+      noName.removeAttribute("data-name-permitted-aria");
+    }
+  }
 }
-removedataNameProhibited()
+removedataNameProhibited();
