99f54b473633f5ab6b0b63edc5d45fe9f6551474
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-09-30 09:06:39 +1000

    Update DL Tests

--- WebExtension/checks/description-lists/description-list-remove.ts
+++ WebExtension/checks/description-lists/description-list-remove.ts
@@ -1,26 +1,22 @@
 "use strict";
 
 for (const element of document.querySelectorAll(
-  ".dl--valid-html-9927845, .dl--valid-div-wrap-dt-dd-9927845, .dl--invalid-first-child-9927845, .dl--invalid-role-9927845, .dl--nested-9927845, .dl--invalid-child-9927845, .dl--single-dt-dd-9927845, [data-isNestedList]"
+  ".dl--valid-html-9927845, .dl--nested-9927845, .dl--invalid-child-9927845, .dl--single-dt-dd-9927845, .dl--invalid-role-9927845, .dl--invalid-childelement-9927845"
 )) {
   element.classList.remove(
     "dl--valid-html-9927845",
-    "dl--valid-div-wrap-dt-dd-9927845",
-    "dl--invalid-first-child-9927845",
-    "dl--single-dt-dd-9927845",
-    "dl--invalid-role-9927845",
     "dl--nested-9927845",
     "dl--invalid-child-9927845",
-    "[data-isNestedList]"
+    "dl--single-dt-dd-9927845",
+    "dl--invalid-role-9927845",
+    "dl--invalid-childelement-9927845"
   );
 }
 
 removeInjectedDivs([
   "html-dl-message-9927845",
-  "div-wrap-message-9927845",
-  "invalid-first-child-message-9927845",
-  "invalid-role-message-9927845",
   "nested-dl-message-9927845",
   "invalid-child-message-9927845",
   "single-dt-dd-message-9927845",
+  "invalid-role-message-9927845",
 ]);
