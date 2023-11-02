0637a6b1b05af768731e97cdfa6c3cda6a5e2545
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-11-02 16:19:11 +1000

    fix bug in dl checks

--- WebExtension/checks/description-lists/description-list-remove.ts
+++ WebExtension/checks/description-lists/description-list-remove.ts
@@ -1,24 +1,27 @@
 "use strict";
 
 for (const element of document.querySelectorAll(
-  ".dl--valid-html-9927845, .dl--nested-9927845, .dl--invalid-child-9927845, .dl--single-dt-dd-9927845, .dl--invalid-role-9927845, .dl--invalid-childelement-9927845, .dl--invalid-dd-9927845"
+  ".dl-nested-9927845, .dl-valid-div-wrap-dt-dd-9927845, .dl-invalid-child-9927845, .dl-valid-html-9927845, .dl-invalid-role-9927845, .dl-single-dt-dd-9927845, .dl-invalid-childelement-9927845, .dl-invalid-dd-9927845",
 )) {
   element.classList.remove(
-    "dl--valid-html-9927845",
-    "dl--nested-9927845",
-    "dl--invalid-child-9927845",
-    "dl--single-dt-dd-9927845",
-    "dl--invalid-role-9927845",
-    "dl--invalid-childelement-9927845",
-    "dl--invalid-dd-9927845"
+    "dl-nested-9927845",
+    "dl-valid-div-wrap-dt-dd-9927845",
+    "dl-invalid-child-9927845",
+    "dl-valid-html-9927845",
+    "dl-invalid-role-9927845",
+    "dl-single-dt-dd-9927845",
+    "dl-invalid-childelement-9927845",
+    "dl-invalid-dd-9927845",
   );
 }
 
 removeInjectedDivs([
-  "html-dl-message-9927845",
   "nested-dl-message-9927845",
+  "dl-div-wrap-message-9927845",
   "invalid-child-message-9927845",
-  "single-dt-dd-message-9927845",
+  "html-dl-message-9927845",
   "invalid-role-message-9927845",
+  "single-dt-dd-message-9927845",
+  "invalid-first-child-message-9927845",
   "invalid-dd-message-9927845",
 ]);
--- WebExtension/checks/description-lists/description-list.ts
+++ WebExtension/checks/description-lists/description-list.ts
@@ -4,19 +4,27 @@ function checkDivChild(child: Element, dlElement: Element): boolean {
   const hasDt = !!child.querySelector("dt");
   const hasDd = !!child.querySelector("dd");
   if (!(hasDt && hasDd)) {
-    dlElement.classList.add("dl--invalid-child-9927845");
+    dlElement.classList.add("dl-invalid-child-9927845");
     const message = `Fail: Invalid DIV without DT and DD detected inside DL.`;
-    createChildMessageDiv(dlElement, "invalid-child-message-9927845", message);
+    createChildMessageDiv(
+      dlElement,
+      "invalid-child-message-9927845",
+      message,
+    );
     return true;
   }
   return false;
 }
 
 function checkOtherChild(child: Element, dlElement: Element): boolean {
-  if (!['DT', 'DD', 'SCRIPT', 'TEMPLATE'].includes(child.nodeName)) {
-    dlElement.classList.add("dl--invalid-childelement-9927845");
+  if (!["DT", "DD", "SCRIPT", "TEMPLATE"].includes(child.nodeName)) {
+    dlElement.classList.add("dl-invalid-childelement-9927845");
     const message = `Fail: Invalid child ${child.nodeName} detected inside DL.`;
-    createChildMessageDiv(dlElement, "invalid-child-message-9927845", message);
+    createChildMessageDiv(
+      dlElement,
+      "invalid-child-message-9927845",
+      message,
+    );
     return true;
   }
   return false;
@@ -25,7 +33,7 @@ function checkOtherChild(child: Element, dlElement: Element): boolean {
 function checkChildStructure(child: Element, dlElement: Element): boolean {
   if (child.nodeName === "DIV") {
     return checkDivChild(child, dlElement);
-  } else if (child.nodeName === "DL") { // Handle nested DLs
+  } else if (child.nodeName === "DL") {
     return checkDLStructure(child);
   } else {
     return checkOtherChild(child, dlElement);
@@ -37,19 +45,21 @@ function checkDLStructure(dlElement: Element): boolean {
   let lastChildWasDt = false;
 
   for (const child of Array.from(dlElement.children)) {
-    // Check if DD immediately follows a DT
     if (child.nodeName === "DD" && !lastChildWasDt) {
-      dlElement.classList.add("dl--invalid-dd-9927845");
+      dlElement.classList.add("dl-invalid-dd-9927845");
       const message = `Fail: DD without preceding DT detected inside DL.`;
-      createChildMessageDiv(dlElement, "invalid-dd-message-9927845", message);
+      createChildMessageDiv(
+        dlElement,
+        "invalid-dd-message-9927845",
+        message,
+      );
       failureDetected = true;
       break;
     }
 
-    lastChildWasDt = (child.nodeName === "DT");
-
-    // Check each child's structure
+    lastChildWasDt = child.nodeName === "DT";
     failureDetected = checkChildStructure(child, dlElement);
+
     if (failureDetected) {
       break;
     }
@@ -60,32 +70,53 @@ function checkDLStructure(dlElement: Element): boolean {
 function checkDescriptionLists(): void {
   const dlElements = document.querySelectorAll("dl");
   for (const dlElement of dlElements) {
-    if (dlElement.parentElement && dlElement.parentElement.nodeName === 'DL') {
-      dlElement.classList.add("dl--nested-9927845");
-      dlElement.setAttribute('data-isNestedList', 'true');
+    // Check for nested DL elements
+    if (dlElement.parentElement && dlElement.parentElement.nodeName === "DL") {
+      dlElement.classList.add("dl-nested-9927845");
       const message = "Fail: Nested DL detected.";
       createChildMessageDiv(dlElement, "nested-dl-message-9927845", message);
       continue;
     }
 
-    let failureDetected = checkDLStructure(dlElement);
+    // Check for DIV wrapping DT and DD
+    const divChild = Array.from(dlElement.children).find(
+      (child) =>
+        child.nodeName === "DIV" &&
+        child.querySelector("dt") &&
+        child.querySelector("dd"),
+    );
+    if (divChild) {
+      dlElement.classList.add("dl-valid-div-wrap-dt-dd-9927845");
+      const message = "Pass: DIV wrapping DT and DD in DL is valid.";
+      createChildMessageDiv(
+        dlElement,
+        "dl-div-wrap-message-9927845",
+        message,
+      );
+      continue;
+    }
 
-    // If a failure was detected in the structure check, skip the remaining checks for this element
+    const failureDetected = checkDLStructure(dlElement);
+
     if (failureDetected) continue;
 
-    // Check for a single DT/DD within a DL
     const dtCount = dlElement.querySelectorAll("dt").length;
     const ddCount = dlElement.querySelectorAll("dd").length;
     if (dtCount === 1 && ddCount === 1) {
-      dlElement.classList.add("dl--single-dt-dd-9927845");
+      dlElement.classList.add("dl-single-dt-dd-9927845");
       const message = "Warning: This DL contains a single key-value pair";
-      createChildMessageDiv(dlElement, "single-dt-dd-message-9927845", message);
+      createChildMessageDiv(
+        dlElement,
+        "single-dt-dd-message-9927845",
+        message,
+      );
     }
 
     // Check for valid HTML Description List
-    const hasHtmlDl = dlElement.nodeName === "DL" && !dlElement.hasAttribute("role");
+    const hasHtmlDl =
+      dlElement.nodeName === "DL" && !dlElement.hasAttribute("role");
     if (hasHtmlDl) {
-      dlElement.classList.add("dl--valid-html-9927845");
+      dlElement.classList.add("dl-valid-html-9927845");
       const message = "Pass: Description List (HTML)";
       createChildMessageDiv(dlElement, "html-dl-message-9927845", message);
     }
@@ -93,13 +124,18 @@ function checkDescriptionLists(): void {
     // Check if the DL element, being a parent, has an invalid role
     const role = dlElement.getAttribute("role");
     if (role && role !== "descriptionlist") {
-      dlElement.classList.add("dl--invalid-role-9927845");
-      const capitalizedRole = role ? role.charAt(0).toUpperCase() + role.slice(1) : null;
+      dlElement.classList.add("dl-invalid-role-9927845");
+      const capitalizedRole = role
+        ? role.charAt(0).toUpperCase() + role.slice(1)
+        : null;
       const message = `Fail: This DL has an invalid role of ${capitalizedRole}`;
-      createChildMessageDiv(dlElement, "invalid-role-message-9927845", message);
+      createChildMessageDiv(
+        dlElement,
+        "invalid-role-message-9927845",
+        message,
+      );
     }
   }
 }
 
-// Call the function to perform the check
 checkDescriptionLists();
