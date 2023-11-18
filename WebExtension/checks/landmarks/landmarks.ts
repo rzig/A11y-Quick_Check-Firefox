c4542c831d683c8c11bb3d44683e0350da9a9627
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-11-18 17:30:13 +1000

    Landmark updates

--- WebExtension/checks/landmarks/landmarks.ts
+++ WebExtension/checks/landmarks/landmarks.ts
@@ -1,42 +1,148 @@
 "use strict";
 
+/**
+ * Main function to add accessibility messages to landmarks in the document.
+ */
 function addLandmarkMessages() {
-  const htmlLandmarks = ["header", "form", "footer", "search" , "nav"];
-  const ariaRoles = ["banner", "form", "contentinfo", "search", "region" , "navigation"];
-
+  const htmlLandmarks = [
+    "article",
+    "aside",
+    "section",
+    "header",
+    "form",
+    "footer",
+    "nav",
+    "main",
+    "search",
+  ];
+  const restrictedParents = ["article", "aside", "main", "nav", "section"];
   const ariaToHtmlMapping: Record<string, string> = {
-    "banner": "header",
-    "form": "form",
-    "contentinfo": "footer",
-    "navigation": "nav",
-    "search": "search"
+    article: "article",
+    section: "section",
+    banner: "header",
+    form: "form",
+    contentinfo: "footer",
+    navigation: "nav",
+    main: "main",
+    aside: "complementary",
+    search: "search",
   };
+
+  /**
+   * Checks if the given element is nested within any of the specified parent elements.
+   * @param element The element to check.
+   * @param parentTags An array of tag names to check against.
+   * @returns true if the element is inside any of the specified parent elements; false otherwise.
+   */
+  function isElementInside(element: Element, parentTags: string[]): boolean {
+    let currentElement = element.parentElement;
+    while (currentElement) {
+      if (parentTags.includes(currentElement.tagName.toLowerCase())) {
+        return true;
+      }
+      currentElement = currentElement.parentElement;
+    }
+    return false;
+  }
+
+  /**
+   * Checks if the HTML role and ARIA role on the same element have different roles and adds a message if they conflict.
+   * @param element The element to check.
+   */
+  function check(element: Element) {
+    const ariaRole = element.getAttribute("role");
+    if (ariaRole) {
+      const tagName = element.tagName.toLowerCase();
+      const htmlRole = ariaToHtmlMapping[ariaRole];
+
+      if (htmlRole && htmlRole !== tagName) {
+        // Remove any existing messages from the element
+        const existingMessages = element.querySelectorAll(
+          ".conflicting-roles-message-88937746"
+        );
+        existingMessages.forEach((message) => {
+          element.removeChild(message);
+        });
+
+        // Add the conflict message to the element
+        const message = `The HTML role '${tagName}' and ARIA role '${ariaRole}' on this element conflict.`;
+        addMessageToPrecedingDiv(
+          element,
+          "conflicting-roles-message-88937746",
+          message
+        );
+      }
+    }
+  }
 
   for (const landmark of htmlLandmarks) {
     const elements = document.querySelectorAll(landmark);
-    const sameTypeLandmarksCount = elements.length + document.querySelectorAll(`[role=${landmark}]`).length;
+    const ariaElements = document.querySelectorAll(`[role=${landmark}]`);
 
     for (const element of elements) {
       if (isHidden(element as HTMLElement)) continue;
 
+      const isInRestrictedParent = isElementInside(element, restrictedParents);
+
+      if (isInRestrictedParent) {
+        const parentTagName = element.parentElement?.tagName.toLowerCase();
+        const message = `Testing with a screen reader, the <${landmark}> role should not be announced when nested inside a <${parentTagName}> element.`;
+
+        const messageClassName = `message-${landmark}-inside-${parentTagName}-88937746`;
+        addMessageToPrecedingDiv(element, messageClassName, message);
+
+        // Add the common class to the message element
+        const messageElement = document.querySelector(`.${messageClassName}`);
+        if (messageElement) {
+          messageElement.classList.add("common-inside-class-88937746");
+        }
+      }
+
+      // Getting direct text content of the element, excluding children
+      let directTextContent = Array.from(element.childNodes)
+        .filter((node) => node.nodeType === Node.TEXT_NODE)
+        .map((node) => (node as Text).textContent?.trim())
+        .join(" ");
+
       let accessibleName = element.getAttribute("aria-label") || "not named";
       let ariaRole = element.getAttribute("role");
-      let hasDuplicateRole = ariaRole && ariaToHtmlMapping[ariaRole] === landmark;
+      let hasDuplicateRole =
+        ariaRole && ariaToHtmlMapping[ariaRole] === landmark;
 
-      let shouldSkipMessage = ["header", "form", "main", "footer"].includes(landmark) && accessibleName === "not named";
+      let shouldSkipMessage = [
+        "header",
+        "form",
+        "main",
+        "footer",
+        "article",
+        "aside",
+        "section",
+      ].includes(landmark);
 
       // Decide the message for HTML landmark
-      let htmlMessage = sameTypeLandmarksCount > 1 && landmark !== "main" ? `HTML ${landmark} landmark has accessible name ${accessibleName}` : "";
-      let htmlMessageClass = `html-${landmark}-message-88937746`;
-      if (!shouldSkipMessage && accessibleName === "not named") {
-        htmlMessage = `HTML ${landmark} landmark is missing an accessible name`;
+      let htmlMessage = "";
+      let htmlMessageClass = "";
+
+      if (!shouldSkipMessage) {
+        if (accessibleName === "not named" && !ariaRole) {
+          htmlMessage = `HTML <${landmark}> landmark is missing an accessible name`;
+        } else {
+          htmlMessage = `HTML <${landmark}> landmark has accessible name ${accessibleName}`;
+        }
+        htmlMessageClass = `html-${landmark}-message-88937746`;
       }
 
       // Decide the message for ARIA role
-      let ariaMessage = ariaRole && (sameTypeLandmarksCount > 1 || ariaRole !== "main") ? `ARIA ${ariaRole} landmark has accessible name ${accessibleName}` : "";
-      let ariaMessageClass = `aria-${ariaRole}-message-88937746`;
-      if (!shouldSkipMessage && accessibleName === "not named") {
-        ariaMessage = ariaRole ? `ARIA ${ariaRole} landmark is missing an accessible name` : "";
+      let ariaMessage = "";
+      let ariaMessageClass = "";
+
+      if (!shouldSkipMessage && ariaRole) {
+        if (accessibleName === "not named" && !ariaRole) {
+          ariaMessage = `ARIA ${ariaRole} landmark is missing an accessible name`;
+        } else {
+          ariaMessage = `ARIA ${ariaRole} landmark has accessible name ${accessibleName}`;
+        }
+        ariaMessageClass = `aria-${ariaRole}-message-88937746`;
       }
 
       // Determine and add the required class for the landmark identified
@@ -44,23 +150,32 @@ function addLandmarkMessages() {
         element.classList.add(`${landmark}--aria-html-88937746`);
       } else if (ariaRole) {
         element.classList.add(`${landmark}--aria--88937746`);
+        // Check for conflicting roles
+        check(element);
       } else {
         element.classList.add(`${landmark}--html--88937746`);
       }
 
-      const rolesThatMustBeNamed = ["navigation", "nav"];  // Add roles and landmarks that must be named
-
+      const rolesThatMustBeNamed = ["navigation", "nav"]; // Add roles and landmarks that must be named
 
       // If there is a duplicate role, add only the duplicate message
       if (hasDuplicateRole) {
         let duplicateMessage = `HTML ${landmark} landmark has a duplicate role in ARIA ${ariaRole!}`;
-        if ((rolesThatMustBeNamed.includes(ariaRole!) || rolesThatMustBeNamed.includes(landmark)) && accessibleName === "not named") {
+        if (
+          (rolesThatMustBeNamed.includes(ariaRole!) ||
+            rolesThatMustBeNamed.includes(landmark)) &&
+          accessibleName === "not named" &&
+          !ariaRole
+        ) {
           duplicateMessage += `. The accessible name is ${accessibleName}`;
         }
         let duplicateMessageClass = `html-aria-duplicate-${landmark}-message-88937746`;
-        addMessageToPrecedingDiv(element, duplicateMessageClass, duplicateMessage);
+        addMessageToPrecedingDiv(
+          element,
+          duplicateMessageClass,
+          duplicateMessage
+        );
       }
-      
 
       // If no duplicate role, then add the decided HTML and ARIA messages
       else {
@@ -70,6 +185,28 @@ function addLandmarkMessages() {
         if (ariaMessage && !shouldSkipMessage) {
           addMessageToPrecedingDiv(element, ariaMessageClass, ariaMessage);
         }
+      }
+    }
+  }
+
+  // Define the common class
+  const commonClass = "common-class-for-aria-only-elements-88937746";
+
+  // Check for elements with ARIA roles but without corresponding HTML landmark roles
+  for (const [ariaRole, htmlEquivalent] of Object.entries(ariaToHtmlMapping)) {
+    const ariaElements = document.querySelectorAll(
+      `[role='${ariaRole}']:not(${htmlEquivalent})`
+    );
+
+    for (const element of ariaElements) {
+      const message = `HTML <${htmlEquivalent}> is fully supported and should be used to replace ARIA role '${ariaRole}'`;
+      const messageClass = `replace-aria-${ariaRole}-with-html-${htmlEquivalent}-88937746`;
+      addMessageToPrecedingDiv(element as HTMLElement, messageClass, message);
+
+      // Add the common class to the preceding div
+      const precedingDiv = element.previousElementSibling;
+      if (precedingDiv) {
+        precedingDiv.classList.add(commonClass);
       }
     }
   }
