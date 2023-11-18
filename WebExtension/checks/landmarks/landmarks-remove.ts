c2b61c8b23152afa86e2d9c191365562d2b86502
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-11-18 17:31:22 +1000

    Last landscape change

--- WebExtension/checks/landmarks/landmarks-remove.ts
+++ WebExtension/checks/landmarks/landmarks-remove.ts
@@ -1,53 +1,68 @@
 "use strict";
 
-// Function to remove the injected divs
-removeInjectedDivs([
-  "html-header-message-88937746",
-  "html-nav-message-88937746",
-  "html-footer-message-88937746",
-  "html-aside-message-88937746",
-  "nav-message-88937746",
+/**
+ * Removes div elements that contain any of the specified message classes.
+ * If the element with the message class is a direct child of a div, the parent div is removed.
+ * Otherwise, the element itself is removed.
+ */
+function removeDivsWithClass() {
+  const messageClasses = [
+    "common-a11y-message-2edbc8ab"
+  ];
 
-  "aria-banner-message-88937746",
-  "aria-navigation-message-88937746",
-  "aria-contentinfo-message-88937746",
-  "aria-complementary-message-88937746",
+  messageClasses.forEach(className => {
+    document.querySelectorAll(`.${className}`).forEach(element => {
+      if (element.parentElement && element.parentElement.tagName === 'DIV') {
+        element.parentElement.remove();
+      } else {
+        element.remove(); // Remove the element itself if it's not in a div.
+      }
+    });
+  });
+}
 
-  "html-aria-duplicate-header-message-88937746",
-  "html-aria-duplicate-nav-message-88937746",
-  "html-aria-duplicate-footer-message-88937746",
-  "html-aria-duplicate-aside-message-88937746"
-]);
+removeDivsWithClass();
 
-// Function to remove the added classes
+/**
+ * Removes specified classes from all elements in the document that have these classes.
+ * This function targets various classes that might have been added by the TypeScript code.
+ */
 function removeAddedClasses() {
   const addedClasses = [
     "header--aria-html-88937746",
     "header--aria--88937746",
     "header--html--88937746",
-
     "nav--aria-html-88937746",
     "nav--aria--88937746",
     "nav--html--88937746",
-
     "footer--aria-html-88937746",
     "footer--aria--88937746",
     "footer--html--88937746",
-
     "aside--aria-html-88937746",
     "aside--aria--88937746",
     "aside--html--88937746",
-    
+    "article--aria-html-88937746",
+    "article--aria--88937746",
+    "article--html--88937746",
+    "section--aria-html-88937746",
+    "section--aria--88937746",
+    "section--html--88937746",
+    "main--aria-html-88937746",
+    "main--aria--88937746",
+    "main--html--88937746",
+    "form--aria-html-88937746",
+    "form--aria--88937746",
+    "form--html--88937746",
+    "search--aria-html-88937746",
+    "search--aria--88937746",
+    "search--html--88937746"
   ];
 
-  // Query all the elements in the DOM that have one of the above classes
-  for (const className of addedClasses) {
-    const elements = document.querySelectorAll(`.${className}`);
-    for (const element of elements) {
-      element.classList.remove(className); // This only removes the specified class
-    }
-  }
+  addedClasses.forEach(className => {
+    document.querySelectorAll(`.${className}`).forEach(element => {
+      element.classList.remove(className);
+    });
+  });
 }
 
-// Call the function to remove the added classes
 removeAddedClasses();
