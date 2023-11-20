806c1c2fe638fa9051851899893bac8c6da224f1
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-11-20 21:08:41 +1000

    Posssible use of screen reader content

--- WebExtension/checks/sr-only/sr-only.ts
+++ WebExtension/checks/sr-only/sr-only.ts
@@ -5,13 +5,16 @@ function checkScreenReaderText() {
   const srOnlyMessageClass = "sr-only-message-identifier";
 
   for (const element of allElements) {
-    const style = window.getComputedStyle(element);
-    if ((style.position === 'absolute' && style.clip !== 'auto') ||
-        (style.width === '1px' && style.height === '1px')) {
+    // Check if the element's text content is not null and not empty or whitespace
+    if (element.textContent && element.textContent.trim() !== "") {
+      const style = window.getComputedStyle(element);
+      if ((style.position === 'absolute' && style.clip !== 'auto') ||
+          (style.width === '1px' && style.height === '1px')) {
 
-      const message = `Screen Reader Only Text Detected: ${element.textContent}`;
-      createChildMessageDiv(element, srOnlyMessageClass, message);
-      element.classList.add("screen-reader-detected");
+        const message = `Possible Screen Reader Only Text Detected: ${element.textContent}`;
+        createChildMessageDiv(element, srOnlyMessageClass, message);
+        element.classList.add("screen-reader-detected");
+      }
     }
   }
 }
