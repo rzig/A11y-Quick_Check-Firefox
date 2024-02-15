2ebf33969ed69cc4cb2104ed9939e7237df5b4a6
Marcus Helvig <git@marcus.helvig.net>
2024-02-16 09:05:56 +1100

    Switch to using a shell script for the normalisation of the source files
    Run linters and prettiers in --fix mode to minimise the formatting difference between changes

--- graft-main.sh
+++ graft-main.sh
@@ -35,7 +35,7 @@ echo Because this does re-write the history, you should know what you are doing
 echo before you run it. This script contains a protection that will rpevent it 
 echo from making any changes.
 
-# exit
+exit
 
 # Get the current remote
 REMOTE_BETA=$(git remote show origin | grep 'Fetch URL' | sed 's#.*\(http.*\)$#\1#')
@@ -140,10 +140,282 @@ cat << --EOF > ../.gitattributes
 *.jpg binary
 --EOF
 
+echo Creating update shell scriopt
+cat << --EOF > ../update-repo.sh
+#! /bin/sh
+
+set -e
+set -u
+
+if [ -z "\${1-}" ] ; then
+    echo "\$1 not set or empty"
+    exit 1
+fi
+
+CURRENT_DIR=\${1-}
+
+echo
+echo Update repo script
+
+echo Creating and adding .gitattributes 
+# Copy the .gitattributes file
+cp \${CURRENT_DIR}/../.gitattributes . 
+
+git add .gitattributes
+
+echo Converting line endings
+
+git ls-files -z | xargs -0 dos2unix -q -r -e -ic0 -- | xargs -0 dos2unix -r -e --
+
+# Normalise the formatting
+
+### Functions to create various files
+
+## package.json
+# stage 1 is before creation of WebExtension folder
+create_package_json_stage1 ()
+{
+cat << !!EOF > package.json
+{
+  "private": true,
+  "scripts": {
+    "stylelint-fix": "stylelint --fix \"**/*.{css,scss}\"",
+    "eslint-fix": "eslint --fix \"**/*.js\"",
+    "prettier-fix": "prettier --ignore-unknown --log-level warn --write \"**/*\""
+  },
+  "devDependencies": {
+    "eslint": "8.52.0",
+    "eslint-config-prettier": "9.0.0",
+    "eslint-config-eslint": "9.0.0",
+    "prettier": "3.0.3",
+    "prettier-plugin-sh": "0.13.1",
+    "sass": "1.69.5",
+    "stylelint": "15.11.0",
+    "stylelint-config-standard-scss": "11.0.0",
+    "typescript": "5.2.2"
+  }
+}
+!!EOF
+}
+
+# stage 1 is before the start of TypeScript conversion
+create_package_json_stage2 ()
+{
+cat << !!EOF > package.json
+{
+  "private": true,
+  "scripts": {
+    "stylelint-fix": "stylelint --fix \"WebExtension/**/*.{css,scss}\"",
+    "eslint-fix": "eslint --fix \"WebExtension/**/*.js\"",
+    "prettier-fix": "prettier --ignore-unknown --log-level warn --write \"WebExtension/**/*\""
+  },
+  "devDependencies": {
+    "eslint": "8.52.0",
+    "eslint-config-prettier": "9.0.0",
+    "eslint-config-eslint": "9.0.0",
+    "prettier": "3.0.3",
+    "prettier-plugin-sh": "0.13.1",
+    "sass": "1.69.5",
+    "stylelint": "15.11.0",
+    "stylelint-config-standard-scss": "11.0.0",
+    "typescript": "5.2.2"
+  }
+}
+!!EOF
+}
+
+## .stylelintignore
+# stage 1 for before creation of the WebExtension folder
+create_stylelintignore_stage1 ()
+{
+cat << !!EOF >  .stylelintignore
+.eslintrc.js
+node_modules
+!!EOF
+}
+
+## .stylelintrc
+# stage 1 for before creation of the WebExtension folder
+create_stylelintrc_stage1 ()
+{
+cat << !!EOF >  .stylelintrc
+{
+  "extends": ["stylelint-config-standard-scss"],
+  "overrides": [
+    {
+      "files": ["WebExtension/**/*.scss"],
+      "customSyntax": "postcss-scss"
+    }
+  ],
+  "rules": {
+	"declaration-block-single-line-max-declarations": null,
+	"selector-class-pattern": null,
+	"unit-no-unknown": null,
+	"font-family-no-missing-generic-family-keyword": null,
+	"no-descending-specificity": null,
+	"custom-property-pattern": null,
+	"no-duplicate-selectors": null,
+	"custom-property-pattern": null,
+	"scss/load-no-partial-leading-underscore": null,
+	"scss/at-mixin-pattern": null,
+  }
+}
+!!EOF
+}
+
+
+## .eslintignore
+# stage 1 for before creation of the WebExtension folder
+create_eslintignore_stage1 ()
+{
+cat << !!EOF >  .eslintignore
+.eslintrc.js
+node_modules
+!!EOF
+}
+
+## eslintrc.js
+# stage 1 for before creation of the WebExtension folder
+create_eslintrc_js_stage1 ()
+{
+cat << !!EOF >  .eslintrc.js
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "prettier",
+  ],
+  env: {
+    es2022: true,
+    browser: true,
+    webextensions: true,
+  },
+  rules: {
+	"no-undef": ["off"],
+	"no-prototype-builtins": ["off"],
+	"no-unused-vars": ["off"],
+	"no-useless-escape": ["off"],
+	"no-cond-assign": ["off"],
+	"no-inner-declarations": ["off"],
+  },
+};
+!!EOF
+}
+
+## .prettierignore
+# stage 1 for before creation of the WebExtension folder
+create_prettierignore_stage1 ()
+{
+cat << !!EOF >  .prettierignore
+.eslintrc.js
+node_modules
+!!EOF
+}
+
+## .prettierrc
+create_prettierrc ()
+{
+cat << !!EOF >  .prettierrc
+{
+  "plugins": ["prettier-plugin-sh"],
+  "printWidth": 80,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": false,
+  "quoteProps": "as-needed",
+  "trailingComma": "all",
+  "bracketSpacing": false,
+  "bracketSameLine": true,
+  "arrowParens": "always",
+  "proseWrap": "always",
+  "htmlWhitespaceSensitivity": "css",
+  "endOfLine": "auto",
+  "embeddedLanguageFormatting": "auto",
+  "singleAttributePerLine": false,
+  "overrides": []
+}
+!!EOF
+}
+
+# Before we moved the location of the shell scripts, things were based
+# in the root directory 
+if [ -f manifest.json ]; then
+	create_package_json_stage1
+	create_stylelintignore_stage1
+	create_stylelintrc_stage1
+	create_eslintignore_stage1
+	create_eslintrc_js_stage1
+	create_prettierignore_stage1
+	create_prettierrc
+
+	npm install -d --no-audit --no-fund --loglevel warn
+
+	# if [ "\$(shasum graft-main.sh)" = "ec8f70df6cc9f0ec9a33c3afcf4ff36878539b21  graft-main.sh" ]
+	#sed -i@ 's/; border 3px/; border: 3px/g' css/headings.css
+	#sed -i~ 's/; border 2px/; border: 2px/g' css/headings.css
+	#sed -i~ 's/^role="heading"]/[role="heading"]/g' css/headings.css
+	#rm css/headings.css~
+
+	#sed -i@ "s/\',0);}//g" css/relatedFormControls.css
+
+	#sed -i@ 's#<div class="the--icon"><img src="icons/icon.svg"</div>#<div class="the--icon"><img src="icons/icon.svg"></div>#g' popup.html
+
+	set +e
+	npm run stylelint-fix
+	npm run eslint-fix
+	npm run prettier-fix
+	set -e
+
+	rm package.json
+	rm package-lock.json
+	rm .stylelintignore
+	rm .stylelintrc
+	rm .eslintignore
+	rm .eslintrc.js
+	rm .prettierignore
+	rm .prettierrc
+	rm -rf node_modules
+# The WebExtension folder has been created
+elif [ -d WebExtension ]; then
+	# At this stage we are still using JavaScript and CSS
+	if [ ! -f WebExtension/package.json -a ! -f package.json ]; then
+		create_package_json_stage2
+		create_stylelintignore_stage1
+		create_stylelintrc_stage1
+		create_eslintignore_stage1
+		create_eslintrc_js_stage1
+		create_prettierignore_stage1
+		create_prettierrc
+
+		npm install -d --no-audit --no-fund --loglevel warn
+
+		set +e
+		npm run stylelint-fix
+		npm run eslint-fix
+		set -e
+		npm run prettier-fix
+
+		rm package.json
+		rm .stylelintignore
+		rm .stylelintrc
+		rm package-lock.json
+		rm .eslintignore
+		rm .eslintrc.js
+		rm .prettierignore
+		rm .prettierrc
+		rm -rf node_modules
+	fi
+fi
+--EOF
+
+chmod +x ../update-repo.sh
+
 CURRENT_DIR=$(pwd)
 
 echo Convert all text files to unix line endings, and add .gitattributes
-FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --tree-filter "cp ${CURRENT_DIR}/../.gitattributes . && git add .gitattributes && git ls-files -z | xargs -0 dos2unix -q -r -e -ic0 -- | xargs -0 dos2unix -r -e --" -- --all
+FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --tree-filter "${CURRENT_DIR}/../update-repo.sh ${CURRENT_DIR}" -- --all
 
 echo Removing backup refs
 rm -rf .git/refs/original
@@ -174,6 +446,7 @@ exit
 #   * Before WebExtension directory
 #   * WebExtension directory, no package.json
 #   * WebExtension directory, package.json in WebExtension
+#   * Some Typescript conversion
 #   * WebExtension directory, package.json in root
 #   * WebExtension directory, package.json in root, linting fix in package.json
 # * Upgrade npm packages
