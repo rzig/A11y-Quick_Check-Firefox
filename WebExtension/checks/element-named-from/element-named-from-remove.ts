4a9bf5899f2774e47748d5c1ff63a528d9c4f16f
Laurence Lewis <laurence.r.lewis@gmail.com>
2024-03-25 12:30:19 +1000

    Fixes bugs for #126

--- WebExtension/checks/element-named-from/element-named-from-remove.ts
+++ WebExtension/checks/element-named-from/element-named-from-remove.ts
@@ -1,15 +1,6 @@
 "use strict";
 
-// for (const element of document.querySelectorAll(
-//   ".valid-9927845, .warning-9927845, .invalid-9927845",
-// )) {
-//   element.classList.remove(
-//     "valid-9927845",
-//     "warning-9927845",
-//     "invalid-9927845",
-//   );
-// }
-
 removeInjectedDivs([
   "neutral-message-9927845",
+  "top-left-container",
 ]);
