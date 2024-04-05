67a1c5bda0eef9f0407aa69083c92a1b6c3927ed
Laurence Lewis <laurence.r.lewis@gmail.com>
2024-04-05 15:04:25 +1000

    Fix bug where span and div children of heading tags are identified as not being coded as headings

--- WebExtension/checks/headings-not-coded/headings-not-coded.ts
+++ WebExtension/checks/headings-not-coded/headings-not-coded.ts
@@ -12,14 +12,16 @@ function checkTextNodesForHeadings(): void {
 
   let node: Node | null;
   while ((node = walk.nextNode())) {
+    // Safely cast parentElement to HTMLElement or skip the iteration if it's null
     const parent = node.parentElement;
+    if (!parent) continue; // This check ensures parent is not null further in the code
 
-    // Continue to the next node if we don't have a parent.
-    if (!parent || isNodeInExcludedContainer(node)) {
+    if (isNodeInExcludedContainer(node)) {
       continue;
     }
 
     const nodeText = node.nodeValue ? node.nodeValue.trim() : "";
+    if (!nodeText) continue; // Additional check to proceed only if nodeText is not empty
     const charCountMessage = `Character count (including spaces) is ${nodeText.length}.`;
 
     const style = window.getComputedStyle(parent);
@@ -31,44 +33,32 @@ function checkTextNodesForHeadings(): void {
     let validTextContainer = false;
 
     while (ancestor) {
-      if (
-        ["h1", "h2", "h3", "h4", "h5", "h6", "svg"].includes(
-          ancestor.tagName.toLowerCase()
-        )
-      ) {
+      if (["h1", "h2", "h3", "h4", "h5", "h6"].includes(ancestor.tagName.toLowerCase())) {
         isHeadingOrSvg = true;
         break;
       }
 
-      if (ancestor.tagName.toLowerCase() === "hgroup") {
-        if (ancestor.querySelectorAll("h1, h2, h3, h4, h5, h6").length > 0) {
-          isHeadingOrSvg = true;
-          break;
-        }
+      if (ancestor.tagName.toLowerCase() === "svg") {
+        isHeadingOrSvg = true;
+        break;
       }
 
-      if (
-        ["div", "span", "p"].includes(ancestor.tagName.toLowerCase()) &&
-        !ancestor.hasAttribute("role")
-      ) {
+      if (["div", "span", "p"].includes(ancestor.tagName.toLowerCase()) && !ancestor.hasAttribute("role")) {
         validTextContainer = true;
         break;
       }
 
-      ancestor = ancestor.parentElement;
+      ancestor = ancestor.parentElement; // It's safe since we've already checked parent is not null
     }
 
-    // Continue to the next node if an ancestor was a heading or SVG, or if the text container is not valid.
     if (isHeadingOrSvg || !validTextContainer) {
       continue;
     }
 
-    // Construct the final message.
     let finalMessage =
       "Needs manual confirmation: Is this a heading? If yes, it is not marked up as a heading in code. " +
       charCountMessage;
 
-    // Apply checks and add messages for valid text containers.
     if (fontSize >= 18 * fontSizeRatio && fontWeight >= 700) {
       parent.classList.add(missingHeadingClass);
       createChildMessageDiv(parent, heavyTextClass, finalMessage);
